e133a33f0c738d3d1f90be87d80ed19f079cf567
Naresh Khatri <nareshkhatri9810@gmail.com>
2024-09-12 23:39:05 +0545

    Design Featured product section

---
+++ src/assets/products/index.js
@@ -0,0 +1,43 @@
+import  cornIcon from './corn.svg'
+import GreenLettuceIcon from './GreenLettuce.svg'
+import GreenAppleIcon from './GreenApple.svg'
+import chaniseCabbageICon from './ChaniseCabbage.svg'
+import ratingIcon from './Rating.svg'
+import greenChilly from './GreenChili.svg'
+
+
+export { ratingIcon}
+export const products=[
+    {
+        image: GreenAppleIcon,
+        title: 'Green Apple',
+        price: '$14.99'
+    },
+    {
+        image:chaniseCabbageICon,
+        title: 'Chanise Cabbage',
+        price: '$14.99',
+        
+    },
+    {
+        image: GreenLettuceIcon,
+        title: 'Green Lettuce',
+        price: '$14.99',
+        
+
+    },
+    {
+        image: cornIcon,
+        title: 'corn',
+        price: '$14.99',
+       
+         
+    },
+    {
+        image: greenChilly,
+        title: 'Green Chili',
+        price: '$14.99',
+    },
+    
+
+]
